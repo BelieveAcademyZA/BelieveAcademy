cff77ff2325274a2f03e97deb53a2a5d2cea29d7
BelieveAcademyZA <159265384+BelieveAcademyZA@users.noreply.github.com>
2024-02-08 08:31:08 +0200

    Add files via upload

---
+++ util.js
@@ -0,0 +1,85 @@
+window.loadGallery = function() {
+	//Get list of media from list.txt
+	fetch("../gallery/list.txt")
+	.then(response => {
+		if(!response.ok) {
+			console.error(response.message);
+		}
+		
+		return response.text();
+	})
+	.then(data => {
+		window.galleryList = data.split("\n");
+	})
+	.catch(error => {
+		console.error(error);
+	});
+}
+
+window.createGallery = function() {
+	if(window.galleryList == null || window.galleryList.length == 0) {
+		loadGallery();
+	}
+	
+	for(var i = 0; i < window.galleryList.length; i++) {
+		var source = window.galleryList[i];
+		
+		//Ignore if comment
+		if(source.startsWith("#")) {
+			continue;
+		}
+		
+		var extention = window.getExtention(source);
+		var type = window.getMediaType(source);
+		
+		//Create element
+		if(type == "img") {
+			var img = document.createElement("img");
+			img.src = source;
+			img.alt = source.split("/").pop().split(".")[0];
+			
+			document.getElementById("gallery").appendChild(img);
+		}
+		if(type == "video") {
+			var video = document.createElement("video");
+			var src = document.createElement("source");
+			src.src = source;
+			src.type = "video/" + extention;
+			video.appendChild(src);
+			
+			document.getElementById("gallery").appendChild(video);
+		}
+	}
+}
+
+window.getExtention(name) {
+	return name.split(".").pop().toLowerCase();
+}
+
+window.getMediaType(name) {
+	//Get extention
+	var extention = window.getExtention(name);
+	
+	var type = "";
+	switch(extention) {
+		case "jpg":
+		case "jpeg":
+		case "png":
+		case "pdf":
+		case "svg":
+		case "gif":
+			type = "img";
+			break;
+		
+		case "mp4":
+		case "webm":
+			type = "video";
+			break;
+		
+		default:
+			console.log("couldn't determine type for \"" + source + "\"");
+			return "Unknown";
+	}
+	
+	return type;
+}
