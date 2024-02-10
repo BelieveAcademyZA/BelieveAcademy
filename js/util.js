7bb503c6e5832239a2473348ec6fd3ec713edd28
BelieveAcademyZA <159265384+BelieveAcademyZA@users.noreply.github.com>
2024-02-10 17:36:31 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -13,7 +13,7 @@ window.addEventListener("DOMContentLoaded", function() {
 
 		//Close if mouse leaves area
 		toggle.parentElement.addEventListener("pointerout", function(evt) {
-			if(evt.target.parentElement.querySelector(".ddMenu").style.display != "none") {
+			if(evt.target.querySelector(".ddMenu").style.display != "none") {
 				toggleDropdown(evt.target);
 			}
 		});
